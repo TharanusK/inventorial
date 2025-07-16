f0fa69b4a930570b3b52ab6c8c06e11f3f20d577
Tharanus <123622257+Tharanus@users.noreply.github.com>
2025-07-16 20:41:38 +0700

    fix: adjust login form width

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "@/components/auth/login-form";
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full justify-center p-6 md:p-10 ">
-      <div className="w-3/4">
+      <div className="w-4/5">
         <LoginForm />
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <div className="flex min-h-svh w-full justify-center p-6 md:p-10 ">
-        <div className="w-3/4">
+        <div className="w-4/5">
           <LoginForm />
         </div>
       </div>
